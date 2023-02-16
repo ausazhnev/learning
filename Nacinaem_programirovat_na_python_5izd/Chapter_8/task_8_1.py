f08985e3968746c06824facb974eaa64b85b3070
Andrey Sazhnev (WarFish) <ausazhnev@gmail.com>
2023-02-16 15:26:23 +0400

    RU Выполнил задачу № 1 глава 8
    EN
    Completed task № 1 chapter 8

---
+++ Nacinaem_programirovat_na_python_5izd/Chapter_8/task_8_1.py
@@ -0,0 +1,32 @@
+# RU
+# Задание №1 к главе 8
+#
+# EN
+# Task №1 to chapter 8
+
+def converter(user_data):
+    initials = ''
+    user_data = user_data.split()
+    for value in user_data:
+        initials += value[0] + '.'
+    return initials
+
+
+def show_initials(initials):
+    print(initials.upper())
+
+
+def get_full_name():
+    full_name = input(f'Введите свои Фамилию Имя Отчество: ')
+    full_name = full_name
+    return full_name
+
+
+def main():
+    full_name = get_full_name()
+    initials = converter(full_name)
+    show_initials(initials)
+
+
+if __name__ == '__main__':
+    main()
