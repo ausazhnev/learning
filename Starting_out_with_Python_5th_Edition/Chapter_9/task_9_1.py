a73ef6e21d895636242364c74750319020e1f53b
warfish <ausazhnev@gmail.com>
2023-02-28 09:48:16 +0400

    Обновление: Добавлены комментарии
    В код добавлены комментарии
    Имена файлов с исходными данными приведены к имени номера задания
    
    [# ---------------- EN ---------------- #]
    Update: Added comments
    
    Comments added to code
    The names of the files with the source data differ from the name of the job number

--- Starting_out_with_Python_5th_Edition/Chapter_9/task_9_1.py
+++ Starting_out_with_Python_5th_Edition/Chapter_9/task_9_1.py
@@ -1,20 +1,27 @@
-# Задание №1 к главе 9
+# Задание #1 к главе 9
 # Текст задания находится в файле tasks_ru.txt
+# Исходные данные для создания словарей находятся в файлах:
+# 'dop/tab_9_1_1.png', 'dop/tab_9_1_3.png', 'dop/tab_9_1_3.png'
 #
-# Task №1 to chapter 9
+# Task # 1 to chapter 9
 # The text of the task is in the file tasks_en.txt
+# The initial data for creating dictionaries is in the files:
+# 'dop/tab_9_1_1.png', 'dop/tab_9_1_3.png', 'dop/tab_9_1_3.png'
 
 CLASSROOM = {'CS101': 3003, 'CS102': 4501, 'CS103': 6755, 'CS104': 1244, 'CS105': 1411}
 TEACHER = {'CS101': 'Hains', 'CS102': 'Alvardo', 'CS103': 'Rich', 'NT110': 'Berk', 'CM241': 'Li'}
 TIME = {'CS101': '8:00', 'CS102': '9:00', 'CS103': '10:00', 'NT110': '11:00', 'CM241': '13:00'}
 
 
+# Получение данных от пользователя
 def get_user_data():
     return input(f'[#] > Введите номер интересующего курса:\n'
                  f'[#] > > '
                  )
 
 
+# Обработка запроса. Создаём новый словарь в который добавляем данные по ключу введенному
+# пользователем если в исходном словаре данных нет, то в новый словарь не чего не добавляется
 def data_processing(data):
     answer = {}
     if data in CLASSROOM:
@@ -26,7 +33,10 @@ def data_processing(data):
     return answer
 
 
+# Вывод данных на экран
 def show_result(answer, user_data):
+    # метод get возвращает значение по его ключу, если значение не найдено,
+    # возвращается значение заданное нами при вызове метода
     print(f'[#] > Данные для курса:\t {user_data}\n'
           f'[#] > Номер аудитории:\t {answer.get("room", "Данные не найдены")}\n'
           f'[#] > Преподаватель:\t {answer.get("teacher", "Данные не найдены")}\n'
