1d14153e195a0c147274ba704adf3da38c369510
warfish <ausazhnev@gmail.com>
2023-03-08 18:45:58 +0400

    Обновил: добавил новые методы
    Описание класса RetailItem обновлено. 'm_retailitem_10_5.py'
     - Добавлен метод-получатель 'get_name' для получения описания товара
     - Добавлен метод-получатель 'get_price' для получения стоимости товара
    
    [# - - - - - - - - - - - - - - -- EN -- - - - - - - - - - - - - - - #]
    Updated: added new methods
    
    The description of the RetailItem class has been updated. 'm_retailitem_10_5.py'
      - Added 'get_name' getter method to get product description
      - Added 'get_price' getter method to get item price

--- Starting_out_with_Python_5th_Edition/Chapter_10/exercises/m_retailitem_10_5.py
+++ Starting_out_with_Python_5th_Edition/Chapter_10/exercises/m_retailitem_10_5.py
@@ -1,10 +1,19 @@
 # Класс RetailItem
 
 class RetailItem:
-    def __init__(self, description=None, price=None, store=None):
+    def __init__(self, description=None, store=None, price=None):
         self.__description = description
         self.__price = price
         self.__store = store
+
+    def get_name(self):
+        return self.__description
+    #
+    # def get_store(self):
+    #     return self.__store
+    #
+    def get_price(self):
+        return self.__price
 
     def __str__(self):
         return f'Описание: {self.__description}, ' \
