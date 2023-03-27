df159d57d23bd828f7cd8e578480182d2cdbd6c1
warfish <ausazhnev@gmail.com>
2023-03-27 09:47:04 +0400

    Исправил: синтаксические ошибки в тексте
    В коде и/или тексте исправлены синтаксические и/или пунктуационные
    ошибки
    
    [# - - - - - - - - - - - - - - -- EN -- - - - - - | - - - - - - - - #]
    Fixed: syntax errors in the text
    
    Syntax and/or punctuation errors have been corrected in the code
    and/or text

--- Starting_out_with_Python_5th_Edition/Chapter_8/task_8_12_v2.py
+++ Starting_out_with_Python_5th_Edition/Chapter_8/task_8_12_v2.py
@@ -6,7 +6,7 @@
 # The text of the task is in the file tasks_en.txt
 # word slice solution
 
-user_string = input(f'[#] > Введите строку для переобразования: ').upper()
-user_string=user_string.split()
+user_string = input(f'[#] > Введите строку для преобразования: ').upper()
+user_string = user_string.split()
 for word in user_string:
-    print(f'{word[1:]}{word[0]}КИ ',sep='', end='')
+    print(f'{word[1:]}{word[0]}КИ ', sep='', end='')
