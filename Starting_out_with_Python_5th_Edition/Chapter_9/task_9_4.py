5b4c5dfb00de83d75afe52e430b0b021a09b06ae
warfish <ausazhnev@gmail.com>
2023-03-27 10:06:13 +0400

    Обновил: логику решения задачи
    Переписал решения задачи разделив ход решения на отдельные функции
    рефакторинг кода
    
    [# - - - - - - - - - - - - - - -- EN -- - - - - - | - - - - - - - - #]
    Updated: the logic of solving the task
    
    Rewrote the solution of the problem by dividing the solution into
    separate functions
    code refactoring

--- Starting_out_with_Python_5th_Edition/Chapter_9/task_9_4.py
+++ Starting_out_with_Python_5th_Edition/Chapter_9/task_9_4.py
@@ -6,31 +6,37 @@
 # The text of the task is in the file tasks_en.txt
 # Source file for analysis 'dop\text_9_4.txt'
 
+# список спецсимволов которые будут удаляться из текста
+MARKS = ':;!@#$%^&*()_-=+<>.,`*?—\\/"\n'
+
+
+# Показать результат на экране
 def show(unique_word):
-    cout = 0
     for word in unique_word:
         print(f'[#] > {word}')
-        cout += 1
-    print(f'[#] > Всего найдено: {cout} уникальных слов')
+    print(f'[#] > Всего найдено: {len(unique_word)} уникальных слов')
+
+
+# Убираем все спец символы из строки и формируем множество для вывода на экран
+def del_marks(unique_word, lines_in_file):
+    for line in lines_in_file:
+        for word in line.split():
+            word = word.strip(MARKS)
+            unique_word.add(word.lower())
+
+
+# Читаем данные из файла
+def get_data():
+    with open('dop/text_9_4.txt', 'r', encoding='utf=8') as file:
+        lines_in_file = file.readlines()
+        return lines_in_file
 
 
 def main():
-    with open('dop\\text_9_4.txt', 'r', encoding='utf=8') as file:
-        unique_word = set()
-        data = file.readlines()
-        for line in data:
-            # убираем все '.'  из строки
-            line = line.replace('.', '')
-            # убираем все ','  из строки
-            line = line.replace(',', '')
-            # убираем все '!'  из строки
-            line = line.replace('!', '')
-            # убираем все '?'  из строки
-            line = line.replace('?', '')
-            line = line.strip('\n')
-            # объединяем что позволяет исключить повторы слов
-            unique_word = unique_word.union(line.split(' '))
-        show(unique_word)
+    unique_word = set()
+    lines_in_file = get_data()
+    del_marks(unique_word, lines_in_file)
+    show(unique_word)
 
 
 if __name__ == '__main__':
